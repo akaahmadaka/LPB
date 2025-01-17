b55f7a4edfdb40c60327c7d1e90b48198f76b8af
akaahmadaka <137176438+akaahmadaka@users.noreply.github.com>
2025-01-17 23:16:43 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import telebot
+import os
+
 # Telegram bot token
-BOT_TOKEN = "745433334:ftdtfgfdr0"
+BOT_TOKEN = "7856502542:AAFOlqazGOG0JpwpQjfmm_ARNd4huACWVpM"
+bot = telebot.TeleBot(BOT_TOKEN)
 
 # List of admin user IDs
-ADMINS = [454323566754]  # Replace with actual admin user IDs
+ADMINS = [5250831809]  # Replace with actual admin user IDs
 
 # Database connection URI
 DATABASE_URI = "sqlite:///links.db"  # SQLite database file
