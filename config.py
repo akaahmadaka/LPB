9c45ab78f376f3309265c83130e08feae7d5c137
akaahmadaka <137176438+akaahmadaka@users.noreply.github.com>
2025-01-17 23:19:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import telebot
 import os
 
 # Telegram bot token
-BOT_TOKEN = "7856502542:AAFOlqazGOG0JpwpQjfmm_ARNd4huACWVpM"
+BOT_TOKEN = "445665433334677"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # List of admin user IDs
-ADMINS = [5250831809]  # Replace with actual admin user IDs
+ADMINS = [3457654446]  # Replace with actual admin user IDs
 
 # Database connection URI
 DATABASE_URI = "sqlite:///links.db"  # SQLite database file
